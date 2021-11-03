acf54b23883e67a09b63cd105bd8b908e6e76afd
Anil Yadav <anil_yadav2@baylor.edu>
2021-11-03 15:02:18 -0500

    ml start

---
+++ ml1.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+
+# print(digits.DESCR)
+# print(digits.data[5])
+# print(digits.target[5])
+# print(digits.images[5])
+
+print(digits.data.shape)
+
+print(digits.target.shape)
+
+
+
+
+
